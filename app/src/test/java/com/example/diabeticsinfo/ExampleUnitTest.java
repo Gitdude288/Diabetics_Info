9bcfcd1b9c2b641a00633481c2f0c13e382abc34
Like-A-Hoss <78040022+Like-A-Hoss@users.noreply.github.com>
2021-02-23 21:04:11 -0600

    Added 3 tests 1.  Blood Sugar Ranges outside of what meters can detect, 2.  Pill counts outside reasonable bounds.  3.  Impossible Blood Presures.

--- app/src/test/java/com/example/diabeticsinfo/ExampleUnitTest.java
+++ app/src/test/java/com/example/diabeticsinfo/ExampleUnitTest.java
@@ -10,8 +10,24 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    int bls = 150;
+    int pillSupply = 90;
+    int sBP = 120;
+    int dBP = 70;
     @Test
-    public void addition_isCorrect() {
+        public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void reasonableBloodSugar(){
+        assertFalse(bls > 500 || bls <50);
+    }
+    @Test
+    public void pillCountTest(){
+        assertFalse(pillSupply > 540 || pillSupply < 0);
+    }
+    @Test
+    public void bpNumberTest(){
+        assertFalse(dBP > sBP);
     }
 }
