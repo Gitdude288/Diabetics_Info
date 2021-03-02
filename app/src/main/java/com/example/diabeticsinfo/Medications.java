4ae96d60b0444c0d46eba47a39ed8838f9d22abe
maewtt <74475071+maewtt@users.noreply.github.com>
2021-03-02 16:49:25 -0700

    WIP: Adding shared preferences to medication class

--- app/src/main/java/com/example/diabeticsinfo/Medications.java
+++ app/src/main/java/com/example/diabeticsinfo/Medications.java
@@ -1,20 +1,49 @@
 package com.example.diabeticsinfo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Map;
+
 public class Medications extends AppCompatActivity {
+
+    SharedPreferences sharedpreferences;
+    public static final String MyPREFERENCES = "Medications" ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medications);
+
+        Map<String, Integer> medsList;
+        
     }
 
     public void goBack(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    public void addMedication(View view) {
+        //Get medication name and amount that user entered
+        EditText editText = (EditText) findViewById(R.id.medicationName);
+        EditText editText2 = (EditText) findViewById(R.id.amount);
+        String med = editText.getText().toString();
+        int amt = Integer.parseInt(editText2.getText().toString());
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putInt(med, amt);
+        editor.apply();
+    }
+
+    public void getMedications(View view) {
+
     }
 }
