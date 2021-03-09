63f430cc012e1ce0fb0bc39edd2d09cb4a540052
Scorpio-555 <mye18006@byui.edu>
2021-03-09 11:22:45 -0700

    blood pressure button on vitals activity now links to blood pressure activity

--- app/src/main/java/com/example/diabeticsinfo/BloodPressure.java
+++ app/src/main/java/com/example/diabeticsinfo/BloodPressure.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class bloodPressure extends AppCompatActivity {
+public class BloodPressure extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/diabeticsinfo/Vitals.java
+++ app/src/main/java/com/example/diabeticsinfo/Vitals.java
@@ -2,7 +2,9 @@ package com.example.diabeticsinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Vitals extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class Vitals extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vitals);
+
     }
+
+    public void goToBloodPressure(View view) {
+        Intent intent = new Intent(this, BloodPressure.class);
+        startActivity(intent);
+    }
+
 }
