c7d9ca9c272b7082dd93ddee09f9b8cd1b962392
Scorpio-555 <mye18006@byui.edu>
2021-03-16 00:26:06 -0600

    GenerateReportActivity has auto generated dates

--- app/src/main/java/com/example/diabeticsinfo/GenerateReportActivity.java
+++ app/src/main/java/com/example/diabeticsinfo/GenerateReportActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Calendar;
 
 public class GenerateReportActivity extends AppCompatActivity {
 
@@ -12,6 +17,31 @@ public class GenerateReportActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_report);
+
+        Calendar today = Calendar.getInstance();
+        Calendar fromDate = Calendar.getInstance();
+        fromDate.add(Calendar.DAY_OF_MONTH, -90);
+
+        String fromMM = String.valueOf(fromDate.get(Calendar.MONTH) + 1);
+        String fromDD = String.valueOf(fromDate.get(Calendar.DAY_OF_MONTH));
+        String fromYY = String.valueOf(fromDate.get(Calendar.YEAR));
+        String toMM = String.valueOf(today.get(Calendar.MONTH) + 1);
+        String toDD = String.valueOf(today.get(Calendar.DAY_OF_MONTH));
+        String toYY = String.valueOf(today.get(Calendar.YEAR));
+
+        EditText fromMonth = findViewById(R.id.fromMonth);
+        EditText fromDay = findViewById(R.id.fromDay);
+        EditText fromYear = findViewById(R.id.fromYear);
+        EditText toMonth = findViewById(R.id.toMonth);
+        EditText toDay = findViewById(R.id.toDay);
+        EditText toYear = findViewById(R.id.toYear);
+
+        fromMonth.setText(fromMM);
+        fromDay.setText(fromDD);
+        fromYear.setText(fromYY);
+        toMonth.setText(toMM);
+        toDay.setText(toDD);
+        toYear.setText(toYY);
     }
 
     public void goBack(View view) {
