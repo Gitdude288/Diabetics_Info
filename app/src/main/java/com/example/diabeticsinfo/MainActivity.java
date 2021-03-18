e1f92943c48f4b5c64bf31c70fb7ebba103a2b1d
Like-A-Hoss <78040022+Like-A-Hoss@users.noreply.github.com>
2021-03-18 17:39:39 -0500

    Created classes to begin the Notification handleing processes.  App creates the channels if needed, Notification will handel the actual notification being sent out.

--- app/src/main/java/com/example/diabeticsinfo/MainActivity.java
+++ app/src/main/java/com/example/diabeticsinfo/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.diabeticsinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import com.google.gson.Gson;
 import android.util.Log;
@@ -11,12 +14,43 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 //Comment 1
+    public static final String CHANNEL_1_ID = "med_note";
+    public static final String CHANNEL_2_ID = "vital_note";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        createNotficationChannels();
     }
 
+    /***
+     * @author Nathan Hoskins
+     * Because of newer versions of Android Notification methods needed to be updated.
+     * The newer system will only activate on newer android versions.
+     */
+    private void createNotficationChannels(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel med_Channel = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Diabetes Medicine",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+
+            med_Channel.setDescription("A Channel for Medicine Notifications");
+
+            NotificationChannel vital_Channel = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "Vital Readings",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            
+            vital_Channel.setDescription("A Channel for Medicine Notifications");
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(med_Channel);
+            manager.createNotificationChannel(vital_Channel);
+        }
+    }
     public void goToVitals(View view) {
         Intent intent = new Intent(this, Vitals.class);
         startActivity(intent);
