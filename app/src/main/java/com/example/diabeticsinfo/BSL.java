267cae8f12004dc52e9e4a473cfa626586a63152
Like-A-Hoss <78040022+Like-A-Hoss@users.noreply.github.com>
2021-03-18 20:05:34 -0500

    Added notification generator to BSL.java Left Java doc to explain how to use.

--- app/src/main/java/com/example/diabeticsinfo/BSL.java
+++ app/src/main/java/com/example/diabeticsinfo/BSL.java
@@ -1,7 +1,9 @@
 package com.example.diabeticsinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,4 +20,26 @@ public class BSL extends AppCompatActivity {
         Intent intent = new Intent(this, Vitals.class);
         startActivity(intent);
     }
+    /***
+     * This is the Vitals alert constructor.
+     * @param v the View screen of the device
+     * @param <title> is the title of the notification
+     * @param <message> is the message to appear in notification
+     * @param <time> is a boolean, for if the message is based on a time like taking morning meds
+     * @author Nate Hoskins
+     *
+     */
+    public <title, message, time> void vitalAlert(View v, String title, String message, Boolean time){
+
+        Notification notification = new NotificationCompat.Builder(this, MainActivity.CHANNEL_2_ID)
+                .setContentTitle(title)
+                .setContentText(message);
+        if(time){
+            ((NotificationCompat.Builder) notification).setSmallIcon(R.drawable.ic_time_not);
+        }
+        else{
+            ((NotificationCompat.Builder) notification).setSmallIcon(R.drawable.ic_warning_not);
+        }
+    }
+
 }
